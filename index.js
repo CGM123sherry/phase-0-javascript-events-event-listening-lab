900745df3328159644b9dc0df33acd3da4ecf4b5
CGM123sherry <g.christinemurithi@gmail.com>
2024-06-24 23:18:01 +0300

    Completed the JS even listing lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+
+ const element = document.getElementById('button');
+
+ if (element) {
+    
+    element.addEventListener('click', function() {
+        
+        console.log('button was clicked!');
+    });
+
+} else {
+    console.error('Element with id "button" not found.');
 }
+}
+addingEventListener();
